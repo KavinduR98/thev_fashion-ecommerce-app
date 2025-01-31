fa6ad500fc4a80dc74417c9812d2d66ea8512e06
@kavinduushan07 <kavinduushan07@gmail.com>
2025-01-31 22:44:44 +0530

    create Hero and LatestCollections sections

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+import { products } from "../assets/frontend_assets/assets";
+
+export const ShopContext = createContext();
+
+const  ShopContextProvider = ({children}) => {
+
+    const currency = 'Rs. ';
+    const delivery_fee = 395;
+
+    const value = {
+        products, currency, delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;

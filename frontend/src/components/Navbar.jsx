362599b4fc5b377fd9f65b6dedd5ca62d29ecc99
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-05 09:48:41 +0530

    create Login & Signup pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
           <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
           
           <div className='relative group'>
-            <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+            <Link to='/login'><img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" /></Link>
             <div className='absolute right-0 hidden pt-4 group-hover:block dropdown-menu'>
               <div className='flex flex-col gap-2 px-5 py-3 text-gray-500 rounded w-36 bg-slate-100'>
                 <p className='cursor-pointer hover:text-black'>My Profile</p>

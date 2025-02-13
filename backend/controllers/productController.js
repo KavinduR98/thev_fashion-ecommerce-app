7f612ec9bc32120b861f73e4db206edd5d24d380
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-13 23:05:16 +0530

    implement get-single product function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -68,7 +68,14 @@ const deleteProduct = async (req, res) => {
 
 // single product info function
 const singleProduct = async (req, res) => {
-
+    try {
+        const { productId } = req.body;
+        const product = await productModel.findById(productId);
+        res.json({ success: true, product });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 export { addProduct, listProduct, deleteProduct, singleProduct }

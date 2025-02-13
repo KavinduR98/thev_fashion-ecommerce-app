edf806b47e5d491633e08198b1fd542a313a76f6
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-13 13:31:36 +0530

    initially implement Product controller functions

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+
+
+// add product function
+const addProduct = async (req, res) => {
+    
+}
+
+// list product function
+const listProduct = async (req, res) => {
+    
+}
+
+// delete product function
+const deleteProduct = async (req, res) => {
+    
+}
+
+// single product info function
+const singleProduct = async (req, res) => {
+    
+}
+
+export {addProduct, listProduct, deleteProduct, singleProduct}

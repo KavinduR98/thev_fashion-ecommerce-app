f568b19ef2a19fe67223683ded5decc29566405f
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-13 13:47:33 +0530

    create Multer middleware for formData

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { loginUser, registerUser, adminLogin } from '../controllers/userController.js';
+import upload from '../middleware/multer.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/register', registerUser);
+userRouter.post('/register', upload.fields([{name: 'image1', maxCount:1}, {name: 'image2', maxCount:1}, {name: 'image3', maxCount:1}, {name: 'image4', maxCount:1}]), registerUser);
 userRouter.post('/login', loginUser);
 userRouter.post('/admin', adminLogin);
 

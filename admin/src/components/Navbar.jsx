4ea837fa28904e67ca1edc1ba72cb22f46ef274c
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-14 23:15:18 +0530

    impl admin-logout function

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { assets } from '../assets/admin_assets/assets'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
     return (
         <div className='flex items-center py-2 px-[4%] justify-between'>
             <img src={assets.logo} alt="" className='w-[max(10%,80px)]'/>
-            <button className='px-5 py-2 text-xs text-white bg-gray-600 rounded-full sm:px-7 sm:py-2 sm:text-sm'>Logout</button>
+            <button onClick={()=>setToken('')} className='px-5 py-2 text-xs text-white bg-gray-600 rounded-full sm:px-7 sm:py-2 sm:text-sm'>Logout</button>
         </div>
     )
 }

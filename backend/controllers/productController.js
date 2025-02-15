5b3bb4b1b1d50c9062b112253170dcf12a5bc9ca
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-15 11:54:11 +0530

    completed Add product page

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ import productModel from '../models/productModel.js';
 // add product function
 const addProduct = async (req, res) => {
     try {
-        const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
 
         const image1 = req.files.image1 && req.files.image1[0]
         const image2 = req.files.image2 && req.files.image2[0]
@@ -26,7 +26,7 @@ const addProduct = async (req, res) => {
             category,
             price: Number(price), // This data is converting to number
             subCategory,
-            bestSeller: bestSeller === "true" ? true : false, // This data is convertin to boolean
+            bestSeller: bestseller === "true" ? true : false, // This data is convertin to boolean
             sizes: JSON.parse(sizes),
             image: imagesUrl,
             date: Date.now()

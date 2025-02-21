eb8a5ca042586403d396bba28dad54ec33f2327a
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-21 15:14:30 +0530

    completed user cart functions

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    cardData: { type: Object, default: {} } // if there is a new user, no card data. Initially Card data is empty.
+    cartData: { type: Object, default: {} } // if there is a new user, no card data. Initially Card data is empty.
 }, { minimize: false })
 
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -11,36 +11,38 @@ const Cart = () => {
     const [cartData, setCartData] = useState([]);
 
     useEffect(() => {
-        const tempData = [];
+        if (products.length > 0) {
+            const tempData = [];
 
-        for(const items in cartItems){
-            for(const item in cartItems[items]){
-                if(cartItems[items][item] > 0){
-                    tempData.push({
-                        _id: items,
-                        size: item,
-                        quantity: cartItems[items][item]
-                    })
+            for (const items in cartItems) {
+                for (const item in cartItems[items]) {
+                    if (cartItems[items][item] > 0) {
+                        tempData.push({
+                            _id: items,
+                            size: item,
+                            quantity: cartItems[items][item]
+                        })
+                    }
                 }
             }
+            setCartData(tempData);
         }
-        setCartData(tempData);
-        
-    }, [cartItems])
+
+    }, [cartItems, products])
 
     return (
         <div className='border-t pt-14'>
             <div className='mb-3 text-2xl'>
-                <Title text1={'YOUR'} text2={'CART'}/>
+                <Title text1={'YOUR'} text2={'CART'} />
             </div>
 
             <div>
                 {
                     cartData.map((item, index) => {
-                        
+
                         const productData = products.find((product) => product._id === item._id);
-                        
-                        return(
+
+                        return (
                             <div key={index} className='grid py-4 text-gray-700 border-t border-b grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
                                 <div className='flex items-start gap-6'>
                                     <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
@@ -52,11 +54,11 @@ const Cart = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <input onChange={(e)=>e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value))} className='px-1 py-1 border max-w-10 sm:max-w-20 sm:px-2' type="number" min={1} defaultValue={item.quantity}/>
-                                <img onClick={()=>updateQuantity(item._id, item.size, 0)} className='w-4 mr-4 cursor-pointer sm:w-5' src={assets.bin_icon} alt="" />
+                                <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value))} className='px-1 py-1 border max-w-10 sm:max-w-20 sm:px-2' type="number" min={1} defaultValue={item.quantity} />
+                                <img onClick={() => updateQuantity(item._id, item.size, 0)} className='w-4 mr-4 cursor-pointer sm:w-5' src={assets.bin_icon} alt="" />
                             </div>
                         )
-                        
+
                     })
                 }
             </div>
@@ -65,7 +67,7 @@ const Cart = () => {
                 <div className='w-full sm:w-[450px]'>
                     <CartTotal />
                     <div className='w-full text-end'>
-                        <button onClick={()=> navigate('/place-order')} className='px-8 py-3 my-8 text-sm text-white bg-black'>PROCEED TO CHECKOUT</button>
+                        <button onClick={() => navigate('/place-order')} className='px-8 py-3 my-8 text-sm text-white bg-black'>PROCEED TO CHECKOUT</button>
                     </div>
                 </div>
             </div>

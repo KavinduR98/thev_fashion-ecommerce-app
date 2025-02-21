fbd0329b5744c4bf1839d095acf71c39c9cd2018
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-21 22:57:15 +0530

    create order functions

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,33 @@
+
+
+// Placing orders using COD Method
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing orders using Stripe Method
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// Placing orders using Razorpay Method
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// All orders data for Admin panel
+const allOrders = async (req, res) => {
+
+}
+
+// User Order Data for frontend
+const userOders = async (req, res) => {
+
+}
+
+// update order status from Admin Panel
+const updateStatus = async (req, res) => {
+
+}
+
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOders, updateStatus }

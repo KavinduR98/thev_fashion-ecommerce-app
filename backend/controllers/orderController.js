d9c00ccb74a3a786a532cc63871a09ca8563f2bc
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-22 11:04:51 +0530

    implement place-order function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,8 +1,33 @@
-
+import orderModel from "../models/orderModel.js";
 
 // Placing orders using COD Method
 const placeOrder = async (req, res) => {
+    try {
+        const { userId, items, amount, address } = req.body;
 
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: 'COD',
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        // Clear cart data 
+        await userModel.findByIdAndUpdate(userId, { cartData: {} })
+
+        res.json({ success: true, message: "Order Placed" })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+
+    }
 }
 
 // Placing orders using Stripe Method

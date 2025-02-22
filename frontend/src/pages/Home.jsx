1e6292a523306e5090f68cd18033abc1d199829f
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-22 21:57:04 +0530

    add spinner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
+import Spinner from '../components/Spinner';
 
 const Home = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate data fetching or component loading
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // Simulate 1 second loading
+
+    return () => clearTimeout(timer); // Cleanup function
+  }, []);
+
+
   return (
     <div>
-      <Hero />
-      <LatestCollection />
-      <BestSeller />
-      <OurPolicy />
-      <NewsletterBox />
+      {loading ? (
+        <Spinner /> // Show spinner when loading
+      ) : (
+        <>
+          <Hero />
+          <LatestCollection />
+          <BestSeller />
+          <OurPolicy />
+          <NewsletterBox />
+        </>
+      )}
     </div>
   )
 }
